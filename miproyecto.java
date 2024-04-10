f73a72e8eaa18743357a08ee0c2088c23177097b
lumoreira <lulii.moreira96@gmail.com>
2024-04-10 20:56:38 -0300

    recupero archivos

--- miproyecto.java
+++ miproyecto.java
@@ -1,4 +1,71 @@
- public class miproyecto {
+ /* 
+public class miproyecto {
+	public static void main(String[] args) {
+		System.out.println("hola mundo");
+	}
+}
+*/
+
+/* 
+//tp 1, consigna 1
+	public class miproyecto {
+		public static void main(String[] args) {
+			int valor= 5;
+			double medida= 7.6;
+			char caracter= 'e';
+			boolean esAlto= true;
+			System.out.println(valor);
+			System.out.println(medida);
+			System.out.println(caracter);
+			System.out.println(esAlto);		
+		}
+	}*/
+/* 
+//consigna 2
+
+	public class miproyecto {
+		public static void main(String[] args) {
+			double cambiar=0;
+			final double num=1.0;
+			System.out.println(cambiar + (num*2));
+		}
+	}*/
+/* 
+	//consigna 3 = no se para que pide una variable constante si no se usa.
+
+import java.util.Scanner;
+
+public class miproyecto {
+		public static void main(String[] args) {
+			final int num;
+			 Scanner scanner = new Scanner(System.in);
+			System.out.println("ingrese un valor");
+			int numero = scanner.nextInt();
+			System.out.println(numero);
+		}
+	}
+*/
+/* 
+//consigna 4
+
+import java.util.Scanner;
+public class miproyecto {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+			System.out.println("ingrese 5 valores distintos");
+			int numero1 = scanner.nextInt();
+			Scanner scanner2 = new Scanner(System.in);
+			int numero2 = scanner.nextInt();
+			int numero3 = scanner.nextInt();
+			int numero4 = scanner.nextInt();
+			int numero5 = scanner.nextInt();
+			System.out.println(numero5 + " " + numero4 + " " + numero3 + " " + numero2 + " " +numero1);
+	}
+}*/
+
+//consigna 5
+
+public class miproyecto {
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
 	}
